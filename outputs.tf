e2e08ad38a9cc825abae6a6b1bbb7e6e26aedc03
Afonso Filho <afonso.rf@gmail.com>
2022-07-14 21:13:48 -0300

    Correcao de descricoes

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "public_ip" {
-  description = "Output of ip public"
+  description = "Acesso via SSH a instancia EC2"
   
   value = format(
     "\n ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l ubuntu -p 22 -i %s %s \n",
@@ -9,7 +9,7 @@ output "public_ip" {
 }
 
 output "ansible_run" {
-  description = "Line of the Ansible Playbook Run"
+  description = "Linha para rodar o Ansible Playbook"
   
   value = format(
     "\n ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ubuntu -i '%s,' --private-key %s ansible/playbook.yml \n", aws_instance.instance.public_ip,
